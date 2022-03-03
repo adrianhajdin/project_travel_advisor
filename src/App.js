93800b1fd7701c3cd1ab89829f1aef993c086d3e
abhi <abhi05042003@outlook.com>
2022-03-03 14:33:54 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ const App = () => {
 
       getPlacesData(type, bounds.sw, bounds.ne)
         .then((data) => {
-          setPlaces(data.filter((place) => place.name && place.num_reviews > 0));
+          setPlaces(data?.filter((place) => place.name && place.num_reviews > 0));
           setFilteredPlaces([]);
           setRating('');
           setIsLoading(false);
